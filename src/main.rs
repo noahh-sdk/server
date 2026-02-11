57485bbee2be9670224f85d6a5ca4a0dcc1d3964
Nipfswd <allaj0412@gmail.com>
2026-02-11 15:21:45 +0100

    docker

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,16 @@ use crate::types::api;
 
 mod endpoints;
 mod types;
-mod auth;
 
 pub struct AppData {
     db: sqlx::postgres::PgPool,
     debug: bool,
-    app_url: String,
-    github_client_id: String,
-    github_client_secret: String
+    app_url: String
 }
 
 #[get("/")]
 async fn health() -> Result<impl Responder, ApiError> {
-    Ok(web::Json("The Noahh Index is running"))
+    Ok(web::Json("The Noah Index is running"))
 }
 
 #[tokio::main]
@@ -28,19 +25,22 @@ async fn main() -> anyhow::Result<()> {
 
     let env_url = dotenvy::var("DATABASE_URL")?;
 
-    let pool = sqlx::postgres::PgPoolOptions::new()
+    let pool = sqlx::postgres::PgPoolOptions::default()
+        .max_connections(10)
         .connect(&env_url).await?;
+    info!("Running migrations");
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await?;
     let addr = "127.0.0.1";
     let port = dotenvy::var("PORT").map_or(8080, |x: String| x.parse::<u16>().unwrap());
     let debug = dotenvy::var("APP_DEBUG").unwrap_or("0".to_string()) == "1";
     let app_url = dotenvy::var("APP_URL").unwrap_or("http://localhost".to_string());
-    let github_client = dotenvy::var("GITHUB_CLIENT_ID").unwrap_or("".to_string());
-    let github_secret = dotenvy::var("GITHUB_CLIENT_SECRET").unwrap_or("".to_string());
 
     info!("Starting server on {}:{}", addr, port);
     let server = HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppData { db: pool.clone(), debug, app_url: app_url.clone(), github_client_id: github_client.clone(), github_client_secret: github_secret.clone() }))
+            .app_data(web::Data::new(AppData { db: pool.clone(), debug, app_url: app_url.clone() }))
             .app_data(QueryConfig::default().error_handler(api::query_error_handler))
             .wrap(Logger::default())
             .service(endpoints::mods::index)
@@ -49,8 +49,6 @@ async fn main() -> anyhow::Result<()> {
             .service(endpoints::mod_versions::get_one)
             .service(endpoints::mod_versions::download_version)
             .service(endpoints::mod_versions::create_version)
-            .service(endpoints::auth::github::poll_github_login)
-            .service(endpoints::auth::github::start_github_login)
             .service(health)
     }).bind((addr, port))?;
 
